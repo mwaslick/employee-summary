e7c0124ca14991bcfb2ad9d51a5cd030927e147b
Maria Waslick <mariawaslick@gmail.com>
2020-10-06 19:14:19 -0400

    got output function working

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const employees = []
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
@@ -13,6 +15,151 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+function generateEmployee() {
+    inquirer.prompt({
+        type: "list",
+        name: "choice",
+        message: "Which type of employee would you like to add?",
+        choices: ["Manager", "Intern", "Engineer", "I'm done"]
+    })
+    .then(function ({ choice }) {
+        switch (choice) {
+            case "Manager":
+                generateManager();
+                break;
+
+            case "Engineer":
+                generateEngineer();
+                break;
+
+            case "Intern":
+                generateIntern();
+                break;
+            
+            case "I'm done":
+                if (employees === []) {
+                    console.log("You must add information for at least one employee in order to generate your team profile.")
+                    generateEmployee()
+                } else {
+                    const rendered = render(employees);
+                    fs.writeFile(outputPath, rendered, function(err) {
+                    if (err) return console.log(err)
+                    });
+                }
+                
+
+            default:
+                console.log("Thank you for using the Employee Generator")
+                break;
+        }
+    })
+}
+
+function generateManager() {
+    inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?"
+    },
+
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's ID?"
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's e-mail address?"
+    },
+
+    {
+        type: "input",
+        name: "officenumber",
+        message: "What is the employee's office number?"
+    }
+])
+.then (function(answers) {
+    const newManager = new Manager(answers.name, answers.id, answers.email, answers.officenumber);
+    employees.push(newManager);
+    console.log(employees)
+    generateEmployee()
+}
+)}
+
+function generateEngineer() {
+    inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?"
+    },
+
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's ID?"
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's e-mail address?"
+    },
+
+    {
+        type: "input",
+        name: "github",
+        message: "What is the employee's GitHub username?"
+    }
+])
+.then (function(answers) {
+    const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    employees.push(newEngineer);
+    console.log(employees)
+    generateEmployee()
+}
+)}
+
+function generateIntern() {
+    inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?"
+    },
+
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's ID?"
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's e-mail address?"
+    },
+
+    {
+        type: "input",
+        name: "school",
+        message: "What school does this employee attend?"
+    }
+])
+.then (function(answers) {
+    const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    employees.push(newIntern);
+    console.log(employees)
+    generateEmployee()
+}
+)}
+
+
+generateEmployee()
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
